e0a8453eccaafca79acabb32e2251a75a08cac31
Thaddee Tyl <thaddee.tyl@gmail.com>
2021-01-29 20:19:04 +0100

    Cache generated blog article HTML render

--- blog/build.sh
+++ blog/build.sh
@@ -3,6 +3,7 @@
 dir=$(dirname "$BASH_SOURCE")
 template=$(cat "$dir"/template.html)
 last_year=$(date -I -d 'last year')
+mkdir -p .cache/html
 
 post_links=
 jsonfeed_items=
@@ -19,39 +20,56 @@ pubcount=$(echo "$publications" | wc -l)
 echo "$publications" | {
   while read post; do
     name=$(echo "$post" | cut -d$'\t' -f2)
+    mdfile="$dir/src/$name".md
+    htmlfile="$dir/posts/$name".html
+    htmlcachefile="$dir/.cache/html/$name".html
+    titlecachefile="$dir/.cache/title/$name".title
+    metacachefile="$dir/.cache/meta/$name".meta
     isotime=$(echo "$post" | cut -d$'\t' -f1)
     last_publication_date="$isotime"
     time=$(date +'%-d %B %Y' -d "$isotime")
-    markdown=$(cat "$dir/src/$name".md)
+
+    # Metadata update.
+    markdown=$(cat "$mdfile")
     title=$(echo "$markdown" | head -1 | sed 's/^# //')
-    content=$(echo "$markdown" | latexmarkdown --body)
-    keywords=$(echo "$markdown" |
-      awk '/^<script type="application\/ld/ {keep=1;next} /^<\/script>/ {keep=0} keep' |
-      jq -r .keywords)
+    meta=$(echo "$markdown" |
+      awk '/^<script type="application\/ld/ {keep=1;next} /^<\/script>/ {keep=0} keep')
+    keywords=$(echo "$meta" | jq -r .keywords)
     tags=$(echo "$keywords" | sed 's/, */ /g')
-    html_tags=$(for k in $tags; do
-      echo " <a class=tag href=\"../index.html?tags=$k\">$k</a>";
-    done | sed '$!s/$/,/')
+
+    # Article page generation.
+    if [[ ! -a "$htmlcachefile" || "$mdfile" -nt "$htmlcachefile" ]]; then
+      echo -n "Generating $name: "; date -Ins
+      # The file has an update; otherwise skip its generation.
+      markdown=${markdown:-$(cat "$mdfile")}
+      content=$(echo "$markdown" | latexmarkdown --body)
+      echo "$content" >"$htmlcachefile"
+      html_tags=$(for k in $tags; do
+        echo " <a class=tag href=\"../index.html?tags=$k\">$k</a>";
+      done | sed '$!s/$/,/')
+      echo -n "$template" \
+        | sed '
+          /TAGS/ {
+            r '<(if [[ "$html_tags" ]]; then
+                echo '      Tags:'"$html_tags".;
+              fi)'
+            d
+          }
+          sTITLE'"$title"'
+          sPUBCOUNT'"$pubcount"'
+          sISOTIME'"$isotime"'
+          sTIME'"$time"'
+          /POST/ {
+            r '<(echo "$content")'
+            d
+          }' \
+        > "$htmlfile"
+    fi
+
+    # Index page generation.
     index_html_tags=$(for k in $tags; do
       echo " <a class=tag href=\"?tags=$k\">$k</a>";
     done | sed '$!s/$/,/')
-    echo -n "$template" \
-      | sed '
-        /TAGS/ {
-          r '<(if [[ "$html_tags" ]]; then
-              echo '      Tags:'"$html_tags".;
-            fi)'
-          d
-        }
-        sTITLE'"$title"'
-        sPUBCOUNT'"$pubcount"'
-        sISOTIME'"$isotime"'
-        sTIME'"$time"'
-        /POST/ {
-          r '<(echo "$content")'
-          d
-        }' \
-      > "$dir"/posts/"$name".html
     post_links=$(cat <<EOF
       <li data-tags="$keywords">
         <a href="posts/$name.html">$title</a>
@@ -63,8 +81,10 @@ $post_links
 EOF
       )
 
+    # RSS feeds generation.
     # We expect RSS feed clients to poll at least once a year.
     if [[ "$isotime" > "$last_year" ]]; then
+      content=$(cat "$htmlcachefile")
       jsonfeed_items=$(cat <<EOF
       {
         "id":  "https://espadrine.github.io/blog/posts/$name.html",
